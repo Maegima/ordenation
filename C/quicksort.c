1e876d95c31069ec7dcc02fcc8e80fe505d6cfa7
Maegima <maegima.andre@gmail.com>
2018-12-19 12:45:11 -0200

    quick sort c

---
+++ C/quicksort.c
@@ -0,0 +1,52 @@
+#include   <stdio.h>
+#include   <stdlib.h>
+#include   "lista.c"
+
+void Particao(Lista *lista, Indice b, Indice e, Indice *i, Indice *j){
+    Item aux, pivo;
+    Item *vec = lista->item;
+    pivo = vec[(e+b)/2];
+    e--;
+    do{
+        while(pivo.chave > vec[b].chave)b++;
+        while(pivo.chave < vec[e].chave)e--;
+        if(b <= e){
+            aux = vec[b];
+            vec[b] = vec[e];
+            vec[e] = aux;
+            b++;
+            e--;
+        }
+    }
+    while(b <= e);
+    (*i) = b;
+    (*j) = e;
+}
+
+void Ordena(Lista *lista, Indice b, Indice e){
+    Indice i, j;
+    Particao(lista, b, e, &i, &j);
+    if(b < j)Ordena(lista, b, j+1);
+    if(i < e)Ordena(lista, i, e);
+}
+
+void QuickSort(Lista *lista){
+    if(!lista) return;
+    Ordena(lista, 0, lista->length);
+}
+
+int main(){
+    int arr[] = {25, 40, 55, 20, 44, 35, 38, 99, 10, 65, 50};
+    Lista *lista;
+    lista = alocaLista(11);
+    Item a;
+    for(int i = 0; i < 11; i++){
+        a.chave = arr[i];
+        InsereItem(lista, a);
+    }
+    imprimeLista(lista);
+    QuickSort(lista);
+    imprimeLista(lista);
+    liberaLista(&lista);
+    return 0;
+}
